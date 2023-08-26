a9706eba827ffe033f3964bb46defd8560c3197d
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-26 15:17:37 +0200

    aggiunto filterName e filterType

--- store.js
+++ store.js
@@ -6,7 +6,7 @@ const store = reactive({
     // 2 - Inserisco Dati da salvare dentro lo Store
     cards: [],
     filterName: '',
-    filterType: ''
+    filterType: '',
 })
 
 // 3 - Rendo i Dati di Store accessibili esportandoli
