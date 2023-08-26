e1cbc92ca621e5d8fb274e2479606efbf9cd586a
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-26 15:20:00 +0200

    caricato filtri in main con emit e fatto funzioni

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,11 +1,13 @@
 <script>
 import axios from 'axios';
+import TheFilters from './TheFilters.vue';
 import TheCard from './TheCard.vue';
 import store from '/store';
 
 export default {
 
     components: {
+        TheFilters,
         TheCard,
     },
 
@@ -20,8 +22,42 @@ export default {
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
                 .then((res) => {
                     this.store.cards = res.data.data;
+                    
                 })
         },
+
+        // Funzione che esegue chiamata al Server con Parametri da componente Filters (Figlio)
+        filterByParam() {
+            
+
+            axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`, {
+                params: {
+                    num: 20,
+                    offset: 0,
+                    fname: store.filterName,
+                    type: store.filterType,
+                    race: store.filterRaceMonster,
+                }
+            })
+                .then((res) => {
+                    this.store.cards = res.data.data;
+                })
+
+                // Con Type='' ho un Errore del Server che intercetto con ".catch" quindi effettuo chiamata solo con paramentro Fname
+                .catch(() => {
+                    axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php`, {
+
+                    params: {
+                        num: 20,
+                        offset: 0,
+                        fname: store.filterName,
+                    }
+                })
+                .then((res) => {
+                    this.store.cards = res.data.data;
+                })
+            })
+        }
     },
 
     mounted() {
@@ -33,6 +69,9 @@ export default {
 
 <template>
     <main>
+        <!-- Ascolto Evento Personalizzato "searchName" e "searchType" (in Figlio) e reagisco con Funzione "filterByName"  -->
+        <TheFilters @searchName="filterByParam()" @searchType="filterByParam()" @searchRaceMonster="filterByParam()"/>
+
         <div class="container bg-white p-4">
             <div class="row row-cols-2 row-cols-lg-6 gap-4 d-flex justify-content-center">
                 <TheCard v-for="card in store.cards" :key="card.id" :srcImg="card.card_images[0].image_url" :name="card.name"
