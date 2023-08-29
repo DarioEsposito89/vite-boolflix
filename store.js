dbf11133e242fdfa256f8f5ba06ed8d0dc2a6176
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-29 11:54:34 +0200

    impostato variabili nello store

--- store.js
+++ store.js
@@ -1,11 +1,9 @@
-// 1 - Creo un Oggetto Reattivo da usare come Store
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
-const store = reactive({
-
-    
-
+export const store = reactive({
+    movieList: [],
+    tvList: [],
+    moviesAPI: 'https://api.themoviedb.org/3/movie/popular?api_key=674396989b918314be769a74712b3d31&page=1',
+    tvAPI: 'https://api.themoviedb.org/3/tv/popular?api_key=674396989b918314be769a74712b3d31&page=1',
+    searchText:"",
 })
-
-// 3 - Rendo i Dati di Store accessibili esportandoli
-export default store;
